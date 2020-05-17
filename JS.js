7669833e1837be2064611845f643ea1361023212
MhmdHourani <m.hourani98@gmail.com>
2020-05-17 12:01:43 +0300

    Basic Changes

--- JS.js
+++ JS.js
@@ -1,4 +1,63 @@
 $(document).ready(function()
 {
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "cars",
+	}).appendTo($("#quastion"));
 
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "carss",
+	}).appendTo($("#quastion"));
+
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "carsss",
+	}).appendTo($("#quastion"));
+
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "carsss",
+	}).appendTo($("#quastion"));
+
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "carsss",
+	}).appendTo($("#quastion"));
+
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "carsss",
+	}).appendTo($("#quastion"));
+
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "carsss",
+	}).appendTo($("#quastion"));
+
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "carsss",
+	}).appendTo($("#quastion"));
+	
+	$("<option/>",{
+		id: idChange(),
+		class :"listOption",
+		text: "carsss",
+	}).appendTo($("#quastion"));
 })
+
+var counter  = -1 ;
+function idChange(){
+	var string = "option";
+	counter++;
+	return (string + counter);
+}
